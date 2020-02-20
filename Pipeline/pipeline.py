ad80792b848756dc3cc472314afff38b057d4b40
Curtis Phan <cdkphan@uwaterloo.ca>
2020-02-19 21:25:13 -0500

    Clean up entry script

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -1,27 +1,14 @@
+#!/usr/bin/env python3
 
 import argparse
 
-
-class FilterCondition(object):
-    OPERATORS = {
-        '==',
-        '!=',
-        '>',
-        '>=',
-        '<',
-        '<=',
-        'and',
-        'or'
-    }
-
-    def __init__(self, tokens):
-        self.tokens = tokens
 
 def positive_int_type(val):
     intval = int(val)
     if intval <= 0:
         raise argparse.ArgumentTypeError("%s is not a positive integer" % value)
     return intval
+
 
 def main():
     parser = argparse.ArgumentParser(description='Arguments')
@@ -30,7 +17,6 @@ def main():
     parser.add_argument('--source', required=True, help='HTTP link to firebase')
     parser.add_argument('--filter', nargs='+', help='A series of tokens representing a condition in python syntax')
     args = parser.parse_args()
-
     
 
 if __name__ == "__main__":
