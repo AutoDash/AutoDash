834be48a8384c7cde297e8aa2529625f2324905c
nicholas-geofroy <nickgeofroy1998@gmail.com>
2020-03-01 21:57:24 +0000

    add storage service with unit tests

--- src/data/VideoItem.py
+++ src/data/VideoItem.py
@@ -1,6 +1,12 @@
+from .MetaDataItem import MetaDataItem
+import hashlib
 class VideoItem:
-    def __init__(self, file_name:str):
+    def __init__(self, file_name:str, meta_data: MetaDataItem):
         self.file_name = file_name
+        self.meta_data = meta_data
+
+    def encode(self):
+        return self.meta_data.encode()
 
     def update_storage_location(self, id: str):
         pass
