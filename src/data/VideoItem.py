84f5385e9bc4fa7a5f2ec2f828fc0d4e5788c7ca
Chaitanya Varier <cvvarier@gmail.com>
2020-03-04 17:22:17 -0800

    updated with additional buffers

--- src/data/VideoItem.py
+++ src/data/VideoItem.py
@@ -1,9 +1,11 @@
 import MetaDataItem
 
 class VideoItem:
-    def __init__(self, file_name:str, video_data:bytes, meta_data:MetaDataItem):
+    def __init__(self, file_name:str, video_data_width:bytes, video_data_height:bytes, video_data_time:bytes, meta_data:MetaDataItem):
         self.file_name = file_name
-        self.video_data = video_data
+        self.video_data_width = video_data_width
+        self.video_data_height = video_data_height
+        self.video_data_time = video_data_time
         self.meta_data = meta_data
 
     def update_storage_location(self, id: str):
