bb5b42bd6e92d711cd2933566c18c3cb0f0b9a06
Chaitanya Varier <cvvarier@gmail.com>
2020-03-04 17:15:24 -0800

    decided on members for VideoItem

--- src/data/VideoItem.py
+++ src/data/VideoItem.py
@@ -1,6 +1,11 @@
+import MetaDataItem
+
 class VideoItem:
-    def __init__(self, file_name:str):
+    def __init__(self, file_name:str, video_data:bytes, meta_data:MetaDataItem):
         self.file_name = file_name
+        self.video_data = video_data
+        self.meta_data = meta_data
 
     def update_storage_location(self, id: str):
         pass
+
