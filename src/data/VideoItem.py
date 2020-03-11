7e1ab4c1ecca7513e3e88f43f17671958d14a675
Chaitanya Varier <cvvarier@gmail.com>
2020-03-11 15:37:44 -0700

    Remove title string, add color channel

--- src/data/VideoItem.py
+++ src/data/VideoItem.py
@@ -1,11 +1,11 @@
 import MetaDataItem
 
 class VideoItem:
-    def __init__(self, file_name:str, video_data_width:bytes, video_data_height:bytes, video_data_time:bytes, meta_data:MetaDataItem):
-        self.file_name = file_name
+    def __init__(self, video_data_width:bytes, video_data_height:bytes, video_data_time:bytes, video_data_rgb:bytes, meta_data:MetaDataItem):
         self.video_data_width = video_data_width
         self.video_data_height = video_data_height
         self.video_data_time = video_data_time
+        self.video_data_rgb = video_data_rgb
         self.meta_data = meta_data
 
     def update_storage_location(self, id: str):
