2c4cd6c11e5bc2cd5180fe5867d41b01fb8c1be4
Ben Upenieks <rupenieks@apple.com>
2020-03-10 21:24:27 -0700

    Wrote example usage of proposed functional API
    Made example usage easier to read

--- src/PipelineConfiguration.py
+++ src/PipelineConfiguration.py
@@ -1,5 +1,59 @@
 import os
 
+"""
+Usage:
+
+    source = Source()
+
+    youtube_dl = YoutubeDownloader(source)
+    reddit_dl = RedditDownloader(source)
+    imgur_dl = ImgurDownloader(source)
+
+    admin = Administrator(youtube_dl, reddit_dl, imgur_dl)
+
+    dashcam_filter = DashcamFilter(admin)
+    accident_filter = AccidentFilter(admin)
+
+    acceptor = Acceptor(dashcam_filter, accident_filter)
+
+    ...
+
+    sink = Sink(*generators)
+
+    pipeline = PipelineConfiguration()
+    pipeline.load_graph(input_nodes=[source], output_nodes=[sink])
+    pipeline.run()
+    pipeline.write("/my/path/myyml.yml")
+
+"""
+
+
+class PipelineNode:
+    parents, children = [], []
+
+    def __init__(self, *parents):
+        self.parents = parents
+        for parent in self.parents:
+            parent._add_child(self)
+            if self._input_type() != parent._output_type():
+                raise ValueError()
+
+    def _add_child(self, child):
+        self.children.append(child)
+
+    def _input_type(self):
+        # return expected input type
+        pass
+
+    def _output_type(self):
+        # return output type
+        pass
+
+    def __call__(self, *args, **kwargs):
+        # run operation
+        pass
+
+
 class PipelineConfiguration:
 
     def __init__(self):
@@ -37,12 +91,12 @@ class PipelineConfiguration:
             'yaml' : self._write_yaml
         })
 
-    def load_graph(self, input_nodes, sink_nodes):
+    def load_graph(self, input_nodes, output_nodes):
         """ 
             Create PipelineConfiguration from existing graph
 
             :param input_nodes: list of PipelineNode objects that generate input to the pipeline - indegree 0
-            :param sink_nodes:  list of PipelineNode objects that handle pipeline output - outdegree 0
+            :param output_nodes:  list of PipelineNode objects that handle pipeline output - outdegree 0
         """
 
         # TODO: DFS / DAG verification
