337b308ed6739f981f46beef431d0d270aaad733
Jack Williamson <jack.williamson@sympatico.ca>
2020-03-30 16:03:53 -0400

    In progress, contains sensitive info so should be squashed
    Current progress adding tags, not complete
    
    Remove sensitive data

--- src/crawler/iCrawler.py
+++ src/crawler/iCrawler.py
@@ -1,14 +1,29 @@
 from abc import ABC, abstractmethod
-from src.data.MetaDataItem import MetaDataItem
+from data.MetaDataItem import MetaDataItem
+from FirebaseAccessor import fetch_video_url_list
+
+class CrawlerException(Exception):
+    '''Raise on inability to find next downloadable'''
+
+class UndefinedDatabaseException(Exception):
+    '''Raise when attempt to access database before injected into the crawler'''
 
 class iCrawler(ABC):
     def __init__(self):
+        self.database = None
         super().__init__
 
     @abstractmethod
     async def next_downloadable(self) -> MetaDataItem:
         pass
 
-    #helper method to publish item to firebase
+    def set_database(self, database):
+        self.database = database
+
+    async def check_new_url(self, url: str) -> bool:
+        urls = await fetch_video_url_list()
+        return url not in urls
+
+    #helper method to publish item to firebase, REMOVE
     def publish_firebase(self, item:MetaDataItem):
         pass
