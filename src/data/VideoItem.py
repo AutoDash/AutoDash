69b59c08290293e3f560474d53843f422155f393
Chaitanya Varier <cvvarier@gmail.com>
2020-04-01 18:49:01 -0700

    remove prefix

--- src/data/VideoItem.py
+++ src/data/VideoItem.py
@@ -2,11 +2,11 @@ from .MetaDataItem import MetaDataItem
 import hashlib
 
 class VideoItem:
-    def __init__(self, video_data_width:bytes, video_data_height:bytes, video_data_time:bytes, video_data_rgb:bytes, meta_data:MetaDataItem):
-        self.video_data_width = video_data_width
-        self.video_data_height = video_data_height
-        self.video_data_time = video_data_time
-        self.video_data_rgb = video_data_rgb
+    def __init__(self, width:bytes, height:bytes, time:bytes, rgb:bytes, meta_data:MetaDataItem):
+        self.width = width
+        self.height = height
+        self.time = time
+        self.rgb = rgb
         self.meta_data = meta_data
         
     def encode(self):
