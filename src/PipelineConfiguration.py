263b10297de600b50b95991e37d0c6aa9aea1e8d
Ben Upenieks <ben.upenieks@uwaterloo.ca>
2020-05-06 15:27:05 -0400

    Added PipelineConfiguration and simple tests

--- src/PipelineConfiguration.py
+++ src/PipelineConfiguration.py
@@ -1,82 +1,75 @@
 import os
+from src.executor.iExecutor import iExecutor
+from typing import List
+import json
+import yaml
+import pickle
 
 """
 Usage:
 
     source = Source()
-
     youtube_dl = YoutubeDownloader(source)
-    reddit_dl = RedditDownloader(source)
-    imgur_dl = ImgurDownloader(source)
-
-    ...
-
-    admin = Administrator(youtube_dl, reddit_dl, imgur_dl)
-
-    dashcam_filter = DashcamFilter(admin)
-    accident_filter = AccidentFilter(admin)
-
-    ...
-
-    acceptor = Acceptor(dashcam_filter, accident_filter)
-
-    ...
-
-    sink = Sink(*generators)
+    dashcam_filter = DashcamFilter(youtube_dl)
+    accident_filter = AccidentFilter(dashcam_filter)
+    sink = Sink(accident_filter)
 
     pipeline = PipelineConfiguration()
-    pipeline.load_graph(input_nodes=[source], output_nodes=[sink])
+    pipeline.load_graph(input_nodes=source, output_nodes=[sink])
     pipeline.run()
     pipeline.write("/my/path/myyml.yml")
 
 """
 
-
-class PipelineNode:
-    parents, children = [], []
-
-    def __init__(self, *parents):
-        self.parents = parents
-        for parent in self.parents:
-            parent._add_child(self)
-            if self._input_type() != parent._output_type():
-                raise ValueError()
-
-    def _add_child(self, child):
-        self.children.append(child)
-
-    def _input_type(self):
-        # return expected input type
-        pass
-
-    def _output_type(self):
-        # return output type
-        pass
-
-    def __call__(self, *args, **kwargs):
-        # run operation
-        pass
-
+class ExecutorFactory:
+    # TODO: this should probably be improved
+    @classmethod
+    def build(cls, executor_name, parent=None):
+        local = {}
+        exec(f'executor_class = {executor_name}', globals(), local)
+        executor_class = local['executor_class']
+        executor = executor_class(parent)
+        return executor
 
 class PipelineConfiguration:
+    """
+        Manages the executor pipeline. Only tree graphs are accepted.
+    """
 
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, ExecutorFactory=ExecutorFactory):
+        self.input_node = None
+        self.output_nodes = None
+        self.graph_dict = None
+        self.ExecutorFactory=ExecutorFactory
+
+    def _read_pickle(self, fpath):
+        with open(fpath, 'rb') as fin:
+            data_dict = pickle.load(fin)
+        self.__dict__.update(data_dict)
+
+    def _write_pickle(self, fpath):
+        with open(fpath, 'wb') as fout:
+            pickle.dump(self.__dict__, fout, 2)
 
     def _read_json(self, fpath):
-        raise NotImplementedError
+        with open(fpath, 'r') as fin:
+            self.graph_dict = json.load(fin)
 
     def _read_yaml(self, fpath):
-        raise NotImplementedError
+        with open(fpath, 'r') as fin:
+            self.graph_dict = yaml.load(fin)
 
     def _write_json(self, fpath):
-        raise NotImplementedError
+        with open(fpath, 'w') as fin:
+            json.dump(self.graph_dict, fin)
 
     def _write_yaml(self, fpath):
-        raise NotImplementedError
+        with open(fpath, 'w') as fin:
+            yaml.dump(self.graph_dict, fin)
 
     def _execute_rw(self, fpath, ffmt, rw_dict):
         if ffmt is None: _, ffmt=os.path.splitext(fpath)
+        ffmt = ffmt.strip('.')
         if ffmt.lower() not in rw_dict: raise ValueError(f"Invalid file format {ffmt.lower()}. Must be one of {rw_dict.keys()}")
 
         rw_dict[ffmt](fpath)
@@ -85,24 +78,74 @@ class PipelineConfiguration:
         self._execute_rw(fpath, ffmt, {
             'json' : self._read_json,
             'yml'  : self._read_yaml,
-            'yaml' : self._read_yaml
+            'yaml' : self._read_yaml,
+            'pkl'  : self._read_pickle
         })
          
     def write(self, fpath, ffmt=None):
         self._execute_rw(fpath, ffmt, {
             'json' : self._write_json,
             'yml'  : self._write_yaml,
-            'yaml' : self._write_yaml
+            'yaml' : self._write_yaml,
+            'pkl'  : self._write_pickle
         })
 
-    def load_graph(self, input_nodes, output_nodes):
+    def load_graph(self, input_node: iExecutor, output_nodes: List[iExecutor]):
         """ 
-            Create PipelineConfiguration from existing graph
+            Create PipelineConfiguration from existing graph. Stores graph in dict as: 
 
-            :param input_nodes: list of PipelineNode objects that generate input to the pipeline - indegree 0
-            :param output_nodes:  list of PipelineNode objects that handle pipeline output - outdegree 0
+            {parent: [{childA: ...}, {childB: ...} ...]}
+
+            :param input_node: iExecutor object that generates input to the pipeline - indegree 0
+            :param output_nodes:  list of iExecutor objects that handle pipeline output - outdegree 0
         """
 
-        # TODO: DFS / DAG verification
-        raise NotImplementedError
+        def collect_nodes(root, visited):
+            if root in visited: 
+                raise RuntimeError("Graph must be a tree.")
+            visited.add(root)
+            root_graph = dict()
+            root_graph[root.get_name()] = []
+            for child in root.next:
+                child_graph = collect_nodes(child, visited)
+                root_graph[root.get_name()].append(child_graph)
 
+            return root_graph
+
+        if input_node is None or output_nodes is None: raise ValueError("Missing input.")
+        if len(output_nodes) == 0: raise ValueError("Missing output nodes.")
+
+        self.input_node = input_node
+        self.output_nodes = output_nodes
+
+        self.graph_dict = collect_nodes(input_node, set())
+
+    def generate_graph(self) -> [iExecutor, List[iExecutor]]:
+        """
+            Return a root executor and output executors from the loaded graph.
+
+            :return [input_executor, [output_executors]]
+        """
+
+        def traverse_and_generate(root_dict, visited, parent=None):
+            root_name = list(root_dict.keys())[0]
+            if root_name in visited: 
+                raise RuntimeError("Graph must be a tree.")
+            visited.add(root_name)
+            
+            root_executor = self.ExecutorFactory.build(executor_name=root_name, parent=parent)
+            if len(root_dict[root_name]) == 0:
+                return [root_executor, [root_executor]]
+
+            sinks = []
+            for child_dict in root_dict[root_name]:
+                _, leaf_executors = traverse_and_generate(child_dict, visited=visited, parent=root_executor)
+                sinks.extend(leaf_executors)
+
+            return [root_executor, sinks]
+
+        if self.graph_dict is None:
+            raise RuntimeError("Graph not loaded.")
+
+        return traverse_and_generate(self.graph_dict, set())
+        
--- src/executor/iExecutor.py
+++ src/executor/iExecutor.py
@@ -1,11 +1,22 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 
 class iExecutor(ABC):
-    def __init__(self, next: 'iExecutor'):
-        self.next = next
+    def __init__(self, prev: 'iExecutor' = None):
+        self.prev = prev
+        self.next = []
+        if prev: prev.add_next(self)
 
     @abstractmethod
     def run(self, obj: Dict[str, Any]):
         pass
+
+    def add_next(self, child):
+        self.next.append(child)
+    
+    def set_prev(self, prev):
+        self.prev = prev
+
+    def get_name(self):
+        return type(self).__name__
