88b30670ea9abf38a4f3f2343a0ba3f54e09a9e0
Tony Meng <tonym321321@gmail.com>
2020-06-04 21:24:25 -0600

    Added support for more than 1 accident location. Added secondary verification UI to check results before continuing

--- src/gui_tool/entry.py
+++ src/gui_tool/entry.py
@@ -1,30 +1,47 @@
 """
-GUI TOOL CONTROLS
+TAGGING GUI CONTROLS
 Navigation:
-    a: 1 back
-    s: 10 back
-    d: 1 forward
-    w: 10 forward
-    space: pause/unpause
+    a:     1 back
+    s:     10 back
+    d:     1 forward
+    w:     10 forward
+    Space: Pause/unpause
+    Enter: Finish and continue
+    Esc:   Abort. Will raise ManualTaggingAbortedException
 
 Tagging:
     m: Mark current location as accident location
     n: Not a dashcam video
         NOTE: by default, all videos will be dashcam
     u: untag (Remove tags)
+    ,: Remove the last marked accident location
+
+REVIEW GUI CONTROLS
+    Enter:     Approve and continue
+    Backspace: Reject and redo
+    Esc:       Abort. Will raise ManualTaggingAbortedException
+    aswd:      Works the same way as in the tagging UI
 
 Note:
     By default, video are assumed to be dashcam video
 
 """
-from src.gui_tool.gui_managers import VideoPlayerGUIManager
+from src.gui_tool.gui_managers import VideoPlayerGUIManager, VideoTaggingContext, VisualizeTaggingResultsGUIManager
 from src.data.MetaDataItem import MetaDataItem
 
 # Lets the user tag the file. Modifies MetaDataItem in place
 def tag_file(file_loc, mdi:MetaDataItem):
-    gui = VideoPlayerGUIManager(file_loc)
-    res = gui.start()
-    mdi.accident_index = res.accident_frame_number
-    mdi.is_dashcam = res.is_dashcam
-    return mdi
+    tagging_results_approved = False
 
+    while not tagging_results_approved:
+        context = VideoTaggingContext(file_loc)
+        gui = VideoPlayerGUIManager(context)
+        gui.start()
+
+        confirmation_gui = VisualizeTaggingResultsGUIManager(context)
+        tagging_results_approved = confirmation_gui.start()
+
+    if context.result.marked:
+        mdi.accident_indexes = context.result.accident_frame_numbers
+        mdi.is_dashcam = context.result.is_dashcam
+    return mdi
--- src/gui_tool/gui_managers.py
+++ src/gui_tool/gui_managers.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
 
+class ManualTaggingAbortedException(Exception):
+    '''Raise when user aborts the tagging'''
+
 class VideoTaggingResults(object):
     def __init__(self):
         self.is_dashcam = None
-        self.accident_frame_number = None
+        self.accident_frame_numbers = None
         self.marked = None
         self.unmark()
 
@@ -13,16 +16,18 @@ class VideoTaggingResults(object):
             return "Unset"
         if not self.is_dashcam:
             return "Is not dashcam"
-        if self.accident_frame_number is None:
+        if len(self.accident_frame_numbers) == 0:
             return "No accident exists"
-        if self.accident_frame_number is not None:
-            return "Accident on frame {0}".format(self.accident_frame_number)
+        if len(self.accident_frame_numbers) > 0:
+            return "Accident on frames {0}".format(self.accident_frame_numbers)
         return "Impossible state"
 
     def mark_accident(self, frame_number: int):
         self.marked = True
-        self.accident_frame_number = frame_number
+        self.accident_frame_numbers.append(frame_number)
 
+    def unmark_last(self):
+        self.accident_frame_numbers = self.accident_frame_numbers[:-1]
 
     def mark_not_dashcam(self):
         self.marked = True
@@ -30,7 +35,7 @@ class VideoTaggingResults(object):
 
     def unmark(self):
         self.is_dashcam = True
-        self.accident_frame_number = None
+        self.accident_frame_numbers = []
         self.marked = False
 
 class VideoCaptureManager(object):
@@ -92,24 +97,38 @@ class VideoCaptureManager(object):
         return self.capture.isOpened() and \
                (self.get_current_frame_index() != self.get_total_frames() or self.get_paused())
 
+    def get_base_dimensions(self):
+        dims = (
+            int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+            int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        )
+        return dims
+
+class VideoTaggingContext(object):
+    def __init__(self, file_loc: str):
+        self.file_loc = file_loc
+        self.vcm = VideoCaptureManager(file_loc)
+        self.vcm.start_from(0)
+        self.result = VideoTaggingResults()
+        self.dim = self.vcm.get_base_dimensions()
+
+
 class VideoPlayerGUIManager(object):
     PROGRESS_BAR_NAME = "progress"
     FRAME_RATE_BAR_NAME = "frame_delay"
     PAUSE_BUTTON_NAME = "pause"
     WINDOW_NAME = 'tagger'
 
-    def __init__(self, file_loc: str):
-        self.vcm = VideoCaptureManager(file_loc)
-        self.vcm.start_from(0)
+    def __init__(self, context: VideoTaggingContext):
+        self.context = context
+        self.vcm = self.context.vcm
         self.frame_rate = 25
-        self.result = VideoTaggingResults()
+        self.result = self.context.result
 
-
-    def start(self) -> VideoTaggingResults:
+    def start(self):
         self.set_GUI()
         self.play_video()
         self.cleanup()
-        return self.result
 
     def set_GUI(self):
         cv2.namedWindow(self.WINDOW_NAME)
@@ -124,7 +143,7 @@ class VideoPlayerGUIManager(object):
             if self.vcm is not None:
                 self.vcm.set_paused(value)
 
-        cv2.createTrackbar(self.PROGRESS_BAR_NAME, self.WINDOW_NAME, 1, self.vcm.get_total_frames(),
+        cv2.createTrackbar(self.PROGRESS_BAR_NAME, self.WINDOW_NAME, 0, self.vcm.get_total_frames()-1,
                            set_progress_rate_callback)
         cv2.createTrackbar(self.FRAME_RATE_BAR_NAME, self.WINDOW_NAME,
                            self.frame_rate, 60, set_frame_rate_callback)
@@ -132,7 +151,7 @@ class VideoPlayerGUIManager(object):
                            False, 1, set_paused_callback)
 
     def play_video(self):
-        while self.vcm.is_open():
+        while True:
             frame = self.vcm.next()
             displayed = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
             cv2.imshow(self.WINDOW_NAME, displayed)
@@ -142,6 +161,8 @@ class VideoPlayerGUIManager(object):
             mark_changed = False
             received_key = cv2.waitKey(self.frame_rate) & 0xFF
             if received_key == 27:  # Escape key
+                raise ManualTaggingAbortedException("Tagging operation aborted")
+            if received_key == 13:  # Enter
                 break
             elif received_key == ord("a"):
                 self.vcm.shift_frame_index(-1)
@@ -162,12 +183,91 @@ class VideoPlayerGUIManager(object):
             elif received_key == ord("m"):
                 self.result.mark_accident(self.vcm.get_current_frame_index())
                 mark_changed = True
+            elif received_key == 44:  # , Key
+                self.result.unmark_last()
+                mark_changed = True
             if mark_changed:
                 print("Marked {0} with {1}".format(self.vcm.file_loc, self.result))
-
 
     def cleanup(self):
         self.vcm.release()
         cv2.destroyAllWindows()
 
+# Displays the tagged frames
+class VisualizeTaggingResultsGUIManager(object):
+    WINDOW_NAME = 'tagging_result_confirmation'
+    FRAME_I_NAME = "Frame"
+    TEXT_STARTING_Y = 17
+    IMG_STARTING_Y = 25
 
+    def __init__(self, context: VideoTaggingContext):
+        self.context = context
+        self.vcm = self.context.vcm
+        self.result = self.context.result
+        self.frame_numbers = self.result.accident_frame_numbers
+
+        self.displayed = None
+
+    def start(self):
+        self.set_GUI()
+        while True:
+            cv2.imshow(self.WINDOW_NAME, self.displayed)
+            received_key = cv2.waitKey(500) & 0xFF
+            if received_key == 27:  # Escape key
+                raise ManualTaggingAbortedException("Verification operation aborted")
+            elif received_key == 13:  # Enter: Accepted
+                return True
+            elif received_key == 8:  # Backspace: Rejected
+                return False
+            elif received_key == ord("a"):
+                self.shift_frame_index(-1)
+            elif received_key == ord("s"):
+                self.shift_frame_index(-10)
+            elif received_key == ord("d"):
+                self.shift_frame_index(1)
+            elif received_key == ord("w"):
+                self.shift_frame_index(10)
+
+    def shift_frame_index(self, shift: int):
+        if len(self.frame_numbers) == 0:
+            return
+        curr_loc = cv2.getTrackbarPos(self.FRAME_I_NAME, self.WINDOW_NAME)
+        new_loc = max(min(curr_loc + shift, len(self.frame_numbers)-1), 0)
+        cv2.setTrackbarPos(self.FRAME_I_NAME, self.WINDOW_NAME, new_loc)
+
+    def set_GUI(self):
+        cv2.namedWindow(self.WINDOW_NAME)
+        cv2.createTrackbar(self.FRAME_I_NAME, self.WINDOW_NAME, 0, len(self.frame_numbers)-1,
+                           self.page_change_callback)
+        self.page_change_callback(0)
+
+    def page_change_callback(self, frame_i: int):
+        img = np.zeros((
+            self.context.dim[0] + self.IMG_STARTING_Y,
+            self.context.dim[1],
+            3), np.uint8)
+        def write_top_text(message):
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            starting_index = (0, self.TEXT_STARTING_Y)
+            font_scale = 0.5
+            font_color = (255, 255, 255)
+            cv2.putText(img, message, starting_index,
+                        font, font_scale, font_color)
+
+        top_txt = "[Dashcam: {0}][Marked: {1}] ".format(
+            self.result.is_dashcam, self.result.marked)
+
+        if len(self.frame_numbers) == 0:
+            top_txt += "No frames were marked as accidents"
+        else:
+            top_txt += "Previewing frame {0} of {1}".format(self.frame_numbers[frame_i], self.frame_numbers)
+        write_top_text(top_txt)
+
+        if len(self.frame_numbers) > 0:
+            self.vcm.start_from(self.frame_numbers[frame_i])
+
+            frame = self.vcm.next()
+            displayed = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
+            img[self.IMG_STARTING_Y:, 0:] = displayed
+
+        self.displayed = img
