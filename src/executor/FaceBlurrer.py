57c7a93f3b10f27f5763868f4af11b3238399b9e
Chaitanya Varier <cvvarier@gmail.com>
2020-07-04 19:57:42 -0400

    add overwrite flag

--- src/executor/FaceBlurrer.py
+++ src/executor/FaceBlurrer.py
@@ -5,7 +5,9 @@ from executor.iExecutor import iExecutor
 
 class FaceBlurrer(iExecutor):
     def run(self, item: VideoItem):
-        # TODO: verify output path
-        blurrer = AutoFaceBlurrer(item.filepath, item.filepath, "0.05", "0.2", "2", "1024")
+        # Run the automatic face blurring tool on the input video with a confidence threshold
+        # of 0.10, enlarge factor of 0.2, skip frames = 1, frame buffer size of 1024, and overwrite = True.
+        # The output video will overwrite the input filepath.
+        blurrer = AutoFaceBlurrer(item.filepath, item.filepath, "0.10", "0.2", "1", "1024", "True")
         blurrer.run()
         return item
