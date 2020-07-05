0d4efacfed367445630dc0642ad53e8a870ad319
Chaitanya Varier <cvvarier@gmail.com>
2020-07-05 17:05:11 -0400

    refactor for adding future tests

--- test/testAnonymizationExecutor.py
+++ test/testAnonymizationExecutor.py
@@ -10,16 +10,14 @@ from numpy.testing import assert_array_equal, assert_raises
 class TestAnonymizationExecutor(unittest.TestCase):
     TEST_DIR = os.path.join(os.getcwd(), "anontest")
     TEST_FILE = "test.mp4"
+    DATASET_PATH = "lib/anonymization/dataset/input"
+    ACCEPTED_FILE_EXTENSION = ".mp4"
     TEST_FILE_PATH = os.path.join(TEST_DIR, TEST_FILE)
 
     def setUp(self):
         # Create test directory and copy one of the test videos from the anonymization repo into it
         if not os.path.exists(self.TEST_DIR):
             os.mkdir(self.TEST_DIR)
-
-        shutil.copy2(os.path.join(os.getcwd(), "lib/anonymization/dataset/input/man_face.mp4"), self.TEST_FILE_PATH)
-        print(self.TEST_FILE_PATH)
-        self.video = VideoItem(self.TEST_FILE_PATH)
 
     def tearDown(self):
         # Delete test directory
@@ -29,12 +27,17 @@ class TestAnonymizationExecutor(unittest.TestCase):
     def test_compiles(self):
         self.assertEqual(True, True)
 
-    def test_face_blurrer(self):
-        original_data = self.video.npy
+    # Test that the executor works with a single video
+    def test_face_blurrer_single(self):
+        # Copy video to test directory
+        shutil.copy2(os.path.join(os.getcwd(), self.DATASET_PATH, "man_face.mp4"), self.TEST_FILE_PATH)
+        video = VideoItem(self.TEST_FILE_PATH)
+
+        original_data = video.npy
 
         # Running the face blurrer should overwrite the input file
         face_blurrer = FaceBlurrer()
-        new_data = face_blurrer.run(self.video)
+        new_data = face_blurrer.run(video)
 
         # Now we check that the video data has changed
         assert_raises(AssertionError, assert_array_equal, original_data, new_data)
