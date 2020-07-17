963e2066b6f959bb832ad66b9aeb1684ebfca07a
Jack Williamson <jack.williamson@sympatico.ca>
2020-07-17 11:24:23 -0400

    Made source stateful and made all database fetching non async

--- src/database/Source.py
+++ src/database/Source.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 from typing import Dict, Any, List
 
 from ..data.FilterCondition import FilterCondition
@@ -10,12 +9,16 @@ from ..executor.iDatabaseExecutor import iDatabaseExecutor
 class Source(iDatabaseExecutor):
 
     def __init__(self, *parents, last_id: str = None, cond: FilterCondition = None):
-        super().__init__(*parents)
+        super().__init__(*parents, stateful=True)
         self.last_id = last_id
         self.cond = cond
+
+        self.data = []
 
     def __load_data(self) -> List[MetaDataItem]:
         return self.database.fetch_newest_videos(self.last_id, self.cond)
 
     def run(self, obj: Dict[str, Any]) -> MetaDataItem:
-        return self.__load_data()[0]
+        if not self.data:
+            self.data = self.__load_data()
+        return self.data.pop(0)
---
+++ test/database/testSource.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import unittest
+
+from src.data.MetaDataItem import MetaDataItem
+from src.database.Source import Source
+from ..mock.MockDataAccessor import MockDataAccessor
+
+class TestSource(unittest.TestCase):
+
+    def test_compiles(self):
+        self.assertEqual(True, True)
+
+    def test_stateful(self):
+        src = Source()
+
+        mockdb = MockDataAccessor()
+        mockdb.publish_new_metadata(MetaDataItem("first", "fake url 1", "youtube"))
+        mockdb.publish_new_metadata(MetaDataItem("second", "fake url 2", "youtube"))
+        src.set_database(mockdb)
+
+        item = src.run({})
+        self.assertEqual("first", item.title)
+
+        item = src.run({})
+        self.assertEqual("second", item.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
