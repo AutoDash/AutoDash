9b427c7b2a2fcaaf9da65ac0c753fb1883df9d52
Nick Geofroy <nicholas.geofroy@uwaterloo.ca>
2020-07-31 13:20:36 -0400

    add tests

--- src/downloader/ImgurDownloader.py
+++ src/downloader/ImgurDownloader.py
@@ -6,7 +6,7 @@ from imgur_downloader import ImgurDownloader as imgur
 import os
 
 class ImgurDownloader(iDownloader):
-    async def download(self, md_idem: MetaDataItem) -> VideoItem:
+    async def download(self, md_item: MetaDataItem) -> VideoItem:
         link = md_item.url
         downloader = imgur(link, dir_download=self.pathname, file_name=link)
         downloader.on_image_download(self.on_download_callback)
