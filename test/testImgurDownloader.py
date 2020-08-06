0d413ae4bbb8c920ea6890a930ba4195ba0c3512
Nick Geofroy <nickgeofroy1998@gmail.com>
2020-08-05 22:10:00 -0400

    Ng/imgur downloader fix (#56)
    * change string to be a metadataItem
    
    * add tests
    
    Co-authored-by: Nick Geofroy <nicholas.geofroy@uwaterloo.ca>

---
+++ test/testImgurDownloader.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import unittest, os
+from src.downloader.ImgurDownloader import ImgurDownloader
+from src.data.MetaDataItem import MetaDataItem
+
+
+class TestImgurDownloader(unittest.TestCase):
+    download_location = os.path.join(os.path.dirname(__file__), "test_data")
+
+    def test_compiles(self):
+        self.assertEqual(True, True)
+
+    def test_download_video(self):
+        file_path = TestImgurDownloader.download_location
+        downloader = ImgurDownloader(pathname=file_path)
+        video_item = downloader.run(MetaDataItem("title","https://imgur.com/r/carcrash/tO6SNIo", "imgur"))
+        vid_filename = os.path.join(file_path,"httpsimgur.comrcarcrashtO6SNIo.mp4")
+        self.assertTrue(os.path.exists(vid_filename))
+        os.remove(vid_filename)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
