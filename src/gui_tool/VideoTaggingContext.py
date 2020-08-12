229db47b546ebe2a1a987d431bd8ddd30e8cd52a
Tony Meng <tonym321321@gmail.com>
2020-08-12 10:06:22 -0600

    Put bounding box fields in a dictionary

--- src/gui_tool/VideoTaggingContext.py
+++ src/gui_tool/VideoTaggingContext.py
@@ -2,6 +2,16 @@
 from .VideoCaptureManager import VideoCaptureManager
 
 class VideoTaggingContext(object):
+    REQUIRED_BBOX_FIELDS =[
+        "frames",
+        "ids",
+        "clss",
+        "x1s",
+        "y1s",
+        "x2s",
+        "y2s",
+        "has_collision",
+    ]
     def __init__(self, file_loc: str, bbox_fields):
         self.file_loc = file_loc
         self.vcm = VideoCaptureManager(file_loc)
@@ -15,16 +25,39 @@ class VideoTaggingContext(object):
         self.is_dashcam = True
         self.additional_tags = {}
 
-    def set_bbox_fields(self, fields):
-        fields = list(fields)
-        for i in range(len(fields)):
-            if fields[i] == None:
-                fields[i] = []
-        assert(len(set([len(f) for f in fields])) == 1)
+    def set_bbox_fields(self, fields: dict):
+        if fields is None:
+            fields = dict()
+            for f in self.REQUIRED_BBOX_FIELDS:
+                fields[f] = []
+
+        assert(all([(x in fields) for x in self.REQUIRED_BBOX_FIELDS]))
+        for key in fields.keys():
+            if fields[key] is None:
+                fields[key] = []
+        assert(len(set([len(f) for f in fields.values()])) == 1)
         self.bbox_fields = fields
+
+    def set_bbox_fields_from_list(self, args):
+        assert(len(args) == len(self.REQUIRED_BBOX_FIELDS))
+        self.set_bbox_fields(dict([
+            (name, val) for name, val in zip(self.REQUIRED_BBOX_FIELDS, args)
+        ]))
 
     def get_bbox_fields(self):
         return self.bbox_fields
+
+    def get_bbox_fields_as_list(self):
+        return [
+            self.bbox_fields["frames"],
+            self.bbox_fields["ids"],
+            self.bbox_fields["clss"],
+            self.bbox_fields["x1s"],
+            self.bbox_fields["y1s"],
+            self.bbox_fields["x2s"],
+            self.bbox_fields["y2s"],
+            self.bbox_fields["has_collision"]
+        ]
 
     def mark_is_dashcam(self, is_dashcam: bool):
         self.marked = True
