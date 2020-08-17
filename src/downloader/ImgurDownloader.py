cbc619f22c1e3606173b513a2e8798e81dcd52a7
Nick Geofroy <nickgeofroy1998@gmail.com>
2020-08-16 21:14:19 -0400

    Ng/imgur downloader albums (#65)
    * imports + videoItem issue
    
    * add album support + checks for mp4

--- src/downloader/ImgurDownloader.py
+++ src/downloader/ImgurDownloader.py
@@ -1,16 +1,43 @@
-from ..downloader.iDownloader import iDownloader
+from ..downloader.iDownloader import iDownloader, DownloadException
 from ..data.VideoItem import VideoItem
 from ..data.MetaDataItem import MetaDataItem
 from imgur_downloader import ImgurDownloader as imgur
-import os
+import os, re, shutil
+
 
 class ImgurDownloader(iDownloader):
+    allowed_chars = re.compile('\W')
+
     async def download(self, md_item: MetaDataItem) -> VideoItem:
         link = md_item.url
-        downloader = imgur(link, dir_download=self.pathname, file_name=link)
-        downloader.on_image_download(self.on_download_callback)
-        downloader.save_images()
-        return VideoItem(filepath=os.path.join(self.pathname, self.file_name), metadata=md_item)
+        filename = self.get_filename(link)
 
-    def on_download_callback(self, pos, url, file):
-        self.file_name = file
+        print(f"downloading file {filename}")
+        downloader = imgur(link, dir_download=self.pathname, file_name=filename)
+        files, idx = downloader.save_images()
+        print(f"downloaded {files}")
+
+        # album downloaded, extract video from album
+        if len(files) > 1:
+            # for each of the files downloaded
+            for file in files:
+                fname, ext = os.path.splitext(file)
+                print(fname, ext)
+                if ext in ['.mp4']:
+                    src = os.path.join(self.pathname, filename, file)
+                    dest = os.path.join(self.pathname,filename) + ".mp4"
+                    print(f"moving from {src} to {dest}")
+                    os.rename(src, dest)
+                    break
+            # remove old directory
+            shutil.rmtree(os.path.join(self.pathname, filename))
+
+        filepath = os.path.join(self.pathname, filename) + ".mp4"
+
+        if not os.path.exists(filepath):
+            raise DownloadException(f"could not download video from {link}")
+
+        return VideoItem(filepath=filepath, metadata=md_item)
+
+    def get_filename(self, link):
+        return ImgurDownloader.allowed_chars.sub('', link)
