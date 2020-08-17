b45e8b47e974ead9387a5037d4c51b2b8f915770
Jack Williamson <jack.williamson@sympatico.ca>
2020-08-17 11:34:18 -0400

    Fixed tests

--- test/testImgurDownloader.py
+++ test/testImgurDownloader.py
@@ -21,16 +21,23 @@ class TestImgurDownloader(unittest.TestCase):
         vid_str = VideoStorageService(file_path)
         downloader = ImgurDownloader()
         downloader.set_video_storage(vid_str)
+
         video_item = downloader.run(MetaDataItem(title="title",url=test_url, download_src="imgur"))
         vid_filename = os.path.join(file_path, expected_title)
         self.assertTrue(os.path.exists(vid_filename))
         os.remove(vid_filename)
 
     def test_download_video_in_album(self):
+        test_url = "https://imgur.com/r/carcrash/EN6aTa6"
+        expected_title = hashlib.sha224(test_url.encode()).hexdigest() + ".mp4"
+
         file_path = TestImgurDownloader.download_location
-        downloader = ImgurDownloader(pathname=file_path)
-        video_item = downloader.run(MetaDataItem(title="title",url="https://imgur.com/r/carcrash/EN6aTa6", download_src="imgur"))
-        vid_filename = os.path.join(file_path,"httpsimgurcomrcarcrashEN6aTa6.mp4")
+        vid_str = VideoStorageService(file_path)
+        downloader = ImgurDownloader()
+        downloader.set_video_storage(vid_str)
+
+        video_item = downloader.run(MetaDataItem(title="title",url=test_url, download_src="imgur"))
+        vid_filename = os.path.join(file_path, expected_title)
         self.assertTrue(os.path.exists(vid_filename))
         os.remove(vid_filename)
 
