28d590b8038e6de7b9219fe0b0b2dad0c8e29cce
Jack Williamson <jack.williamson@sympatico.ca>
2021-01-20 19:35:16 -0500

    Added script for manually looking at firebase data

--- tools/check_firebase.py
+++ tools/check_firebase.py
@@ -1,8 +1,16 @@
 from src.database.FirebaseAccessor import FirebaseAccessor
+from src.data.FilterCondition import FilterCondition
+
+# Firebase Scripts
 
 def get_num_metadata_item():
     f = FirebaseAccessor()
     ids = f.fetch_video_id_list()
     return len(ids)
 
-print(get_num_metadata_item())
+def get_metadata_with_url(url):
+    f = FirebaseAccessor()
+    fc = FilterCondition("url == '{}'".format(url))
+    data = f.fetch_newest_videos(filter_cond=fc)
+    return data
+
