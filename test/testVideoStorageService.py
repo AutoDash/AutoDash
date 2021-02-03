4d9369a25e9dc2a7a28f89d4151467df9bd73d66
Nick Geofroy <nickgeofroy1998@gmail.com>
2021-02-03 18:14:09 -0500

    use pytest for testing

--- test/testVideoStorageService.py
+++ test/testVideoStorageService.py
@@ -3,17 +3,25 @@ import unittest, os
 from src.VideoStorageService import VideoStorageService
 from src.data.VideoItem import VideoItem
 from src.data.MetaDataItem import MetaDataItem
+import pytest
 
+
+@pytest.mark.usefixtures("tmpdir")
 class TestVideoStorageService(unittest.TestCase):
-    def setUp(self):
-        self.storage = VideoStorageService()
+    @pytest.fixture(autouse=True)
+    def initDir(self, tmpdir):
+        self.storage = VideoStorageService(tmpdir)
 
     def test_storage_file_created(self):
         self.assertTrue(os.path.exists(os.path.join(os.getcwd(), "storage")))
 
     def test_store_load_delete_video(self):
-        metaData = MetaDataItem(title="title", url="url", download_src="youtube", 
-                collision_type="car-v-car", description="desc", location="loc")
+        metaData = MetaDataItem(title="title",
+                                url="url",
+                                download_src="youtube",
+                                collision_type="car-v-car",
+                                description="desc",
+                                location="loc")
         vidItem = VideoItem(metadata=metaData)
 
         self.storage.store_video(vidItem)
@@ -30,12 +38,13 @@ class TestVideoStorageService(unittest.TestCase):
 
         for vid in videos:
             self.storage.store_video(
-                VideoItem(metadata=MetaDataItem(
-                        title="title", url=vid, download_src="youtube",
-                        collision_type="comp", description="desc", location="loc"
-                    ), filepath=None
-                )
-            )
+                VideoItem(metadata=MetaDataItem(title="title",
+                                                url=vid,
+                                                download_src="youtube",
+                                                collision_type="comp",
+                                                description="desc",
+                                                location="loc"),
+                          filepath=None))
 
         for vid in self.storage.list_videos():
             videos.remove(vid.metadata.url)
@@ -45,11 +54,11 @@ class TestVideoStorageService(unittest.TestCase):
         self.storage.delete_all()
         self.assertTrue(
             len([x for x in self.storage.list_videos()]) == 0,
-            "Not all videos deleted"
-        )
+            "Not all videos deleted")
 
     def test_delete_all(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
